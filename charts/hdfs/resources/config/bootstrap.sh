36e64d1c08b5e70acb301d829bb50491a391db94
itzhang89 <itzhang89@163.com>
2022-08-16 10:53:02 +0800

    [hadoop] add a nodemanager and resourcemanager for the hadoop cluster

--- charts/hdfs/resources/config/bootstrap.sh
+++ charts/hdfs/resources/config/bootstrap.sh
@@ -17,31 +17,77 @@ for f in slaves core-site.xml hdfs-site.xml mapred-site.xml yarn-site.xml httpfs
     fi
 done
 
+# create hdfs, yarn, hadoop group if not existed
+[[ ! $(getent group hadoop) ]] && groupadd -g 999 -r hadoop
+[[ ! $(getent group hdfs) ]] && groupadd -g 991 -r hdfs
+[[ ! $(getent group yarn) ]] && groupadd -g 995 -r yarn
+# create hdfs and yarn user if not existed
+[[ ! $(id hdfs) ]] && useradd --comment "HDFS User" -u 992 --shell /bin/bash -M -r -N --groups hadoop,hdfs --home /var/lib/hadoop/hdfs hdfs
+[[ ! $(id yarn) ]] && useradd --comment "Yarn User" -u 996 --shell /bin/bash -M -r -N --groups hadoop,yarn --home /var/lib/hadoop/yarn yarn
+
 # installing libraries if any - (resource urls added comma separated to the ACP system variable)
 cd $HADOOP_HOME/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+
 if [[ $2 == "namenode" ]]; then
-    if [ ! -d "/dfs/name" ]; then
-    mkdir -p /dfs/name
+    [[ ! -d  /data/dfs/name ]] && mkdir -p /data/dfs/name && chown -R hdfs:hadoop /data/dfs && chmod g+s /data/dfs
     $HADOOP_HOME/bin/hdfs namenode -format -force -nonInteractive
-    fi
     $HADOOP_HOME/sbin/hadoop-daemon.sh start namenode
 fi
+
 if [[ $2 == "datanode" ]]; then
-    if [ ! -d "/dfs/data" ]; then
-    mkdir -p /dfs/data
-    fi
+    [[ ! -d  /data/dfs/data ]] && mkdir -p  /data/dfs/data && chown -R hdfs:hadoop /data/dfs && chmod g+s /data/dfs
     #  wait up to 30 seconds for namenode
     (while [[ $count -lt 15 && -z $(curl -sf http://{{ include "hdfs.fullname" . }}-namenode:9870) && -z $(curl -sf http://{{ include "hdfs.fullname" . }}-namenode:50070) ]]; do ((count=count+1)) ; echo "Waiting for {{ include "hdfs.fullname" . }}-namenode" ; sleep 2; done && [[ $count -lt 15 ]])
     [[ $? -ne 0 ]] && echo "Timeout waiting for hdfs namenode, exiting." && exit 1
 
     $HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
 fi
+
+# ------------------------------------------------------
+# Start RESOURCE MANAGER and PROXY SERVER as daemons
+# ------------------------------------------------------
+if [[ $2 == "resourcemanager" ]]; then
+$HADOOP_HOME/bin/yarn --loglevel {{ .Values.logLevel }} --daemon start resourcemanager
+$HADOOP_HOME/bin/yarn --loglevel {{ .Values.logLevel }} --daemon start proxyserver
+fi
+# ------------------------------------------------------
+# Start NODE MANAGER
+# ------------------------------------------------------
+if [[ $2 == "nodemanager" ]]; then
+  [[ ! -d /data ]] && mkdir /data
+  sed -i '/<\/configuration>/d' $HADOOP_HOME/etc/hadoop/yarn-site.xml
+  cat >> $HADOOP_HOME/etc/hadoop/yarn-site.xml <<- EOM
+  <property>
+    <name>yarn.nodemanager.resource.memory-mb</name>
+    <value>${MY_MEM_LIMIT:-2048}</value>
+  </property>
+  <property>
+    <name>yarn.nodemanager.resource.cpu-vcores</name>
+    <value>${MY_CPU_LIMIT:-2}</value>
+  </property>
+EOM
+  echo '</configuration>' >> $HADOOP_HOME/etc/hadoop/yarn-site.xml
+  # Wait with timeout for resourcemanager
+  TMP_URL="http://{{ include "hdfs.fullname" . }}-resourcemanager:8088/ws/v1/cluster/info"
+  if timeout 5m bash -c "until curl -sf $TMP_URL; do echo Waiting for $TMP_URL; sleep 5; done"; then
+    $HADOOP_HOME/bin/yarn nodemanager --loglevel {{ .Values.logLevel }}
+  else
+    echo "$0: Timeout waiting for $TMP_URL, exiting."
+    exit 1
+  fi
+fi
+# ------------------------------------------------------
+# Tail logfiles for daemonized workloads (parameter -d)
+# ------------------------------------------------------
 if [[ $1 == "-d" ]]; then
     until find ${HADOOP_HOME}/logs -mmin -1 | egrep -q '.*'; echo "`date`: Waiting for logs..." ; do sleep 2 ; done
     tail -F ${HADOOP_HOME}/logs/* &
     while true; do sleep 1000; done
 fi
 
+# ------------------------------------------------------
+# Start bash if requested (parameter -bash)
+# ------------------------------------------------------
 if [[ $1 == "-bash" ]]; then
     /bin/bash
 fi
